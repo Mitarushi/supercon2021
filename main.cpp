5e35ed54bbae796f28f0470a68a315810ae65ee0
Mitarushi <60428311+Mitarushi@users.noreply.github.com>
2021-06-03 18:41:19 +0900

    add YES/NO check (バグ検査はしていない)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
+#include <array>
 
 int n;
 std::vector<std::pair<int, int>> obstacle; // obstacle[i] := i番目の障害物の座礁(y,x)
@@ -9,7 +11,36 @@ std::vector<std::vector<char>> obstacle_map; // obstacle_map[y][x] := 座標(y,x
 int m;
 std::vector<std::pair<int, int>> operation_shift; // operation[i] := i番目の操作をした時、どれだけ動くか(y,x)
 std::vector<std::vector<std::vector<char>>> edge_map; // edge_map[y][x][i] := 座標(y,x)で操作iが使えるか
+std::vector<std::vector<char>> need_map; // need_map[y][x] := 座標(y,x)に到達する必要があるか
+int need_count; // 訪れる必要がある場所の数 探索の終了判定に
 
+
+void init_need_map() {
+    need_map.resize(n, std::vector(n, (char) 0));
+    need_count = 0;
+
+    std::stack<std::pair<int, int>> stack;
+    stack.emplace(0, 0);
+    need_map[0][0] = 1;
+
+    const std::array<int, 4>
+            y_shift = {-1, 0, 1, 0},
+            x_shift = {0, 1, 0, -1};
+
+    while (!stack.empty()) {
+        auto[y, x] = stack.top();
+        stack.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + y_shift[i], nx = x + x_shift[i];
+            if (0 <= ny && ny < n && 0 <= nx && nx < n && need_map[ny][nx] == 0 && obstacle_map[ny][nx] == 0) {
+                need_map[ny][nx] = 1;
+                stack.emplace(ny, nx);
+                need_count++;
+            }
+        }
+    }
+}
 
 void read_graph() {
     std::cin >> n;
@@ -82,4 +113,36 @@ void read_graph() {
             }
         }
     }
+
+    init_need_map();
+}
+
+
+// 全ての場所に到達可能か
+bool is_ok() {
+    std::stack<std::pair<int, int>> stack;
+    std::vector<std::vector<char>> visited(n, std::vector(n, (char) 0))
+    stack.emplace(0, 0);
+    visited[0][0] = 1;
+    int count = 1;
+
+    while (!stack.empty()) {
+        auto[y, x] = stack.top();
+        stack.pop();
+
+        for (int i = 0; i < m; i++) {
+            if (!edge_map[y][x][i]) {
+                continue;
+            }
+
+            int ny = y + operation_shift[i].first, nx = x + operation_shift[i].second;
+            if (visited[ny][nx] == 0) {
+                stack.emplace(ny, nx);
+                visited[ny][nx] = 1;
+                count++;
+            }
+        }
+    }
+
+    return count == need_count;
 }
