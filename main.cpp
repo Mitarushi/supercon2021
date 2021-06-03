fa4080600e8373fdb3b970ac4013bc28a32f8732
Mitarushi <s153@D56.nada.local>
2021-06-03 16:49:55 +0900

    add read move(tsugi ha tansaku wo jissou sitaine)

--- main.cpp
+++ main.cpp
@@ -1,30 +1,78 @@
 // バグると嫌なので、bits/stdc++.h や using namespace std などは使わない方針で
-#include <vector>
 #include <iostream>
 #include <string>
+#include <vector>
 
-struct graph {
-    int n;
-    std::vector<std::vector<int>> obstacle;
-    int m;
+int n;
+std::vector<std::pair<int, int>> obstacle;
+std::vector<std::vector<int>> obstacle_map;
+int m;
+std::vector<std::vector<std::vector<int>>> edge_map;
 
-    void read_graph() {
-        std::cin >> n;
-        obstacle.resize(n, std::vector<int>(n, 0));
+void read_graph() {
+    std::cin >> n;
+    obstacle.clear();
+    obstacle_map.resize(n, std::vector(n, 0));
 
-        for (int i = 0; i < n; i++) {
-            int y, x;
-            std::cin >> y >> x;
-            obstacle[y - 1][x - 1] = 1;
-        }
-
-        std::cin >> m;
-
-        for (int i = 0; i < m; i++) {
-            std::string move;
-            std::cin >> move;
-        }
-
+    for (int i = 0; i < n; i++) {
+        int y, x;
+        std::cin >> y >> x;
+        y--;
+        x--;
+        obstacle.emplace_back(y, x);
+        obstacle_map[y][x] = 1;
     }
 
-};
+    std::cin >> m;
+    edge_map.resize(n, std::vector(n, std::vector(m, 1)));
+
+    for (const auto& [py, px] : obstacle) {
+        for (int i = 0; i < m; i++) {
+            edge_map[py][px][i] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        std::string move;
+        std::cin >> move;
+
+        int y = 0, x = 0, ymax = 0, ymin = 0, xmax = 0, xmin = 0;
+        for (const char& j : move) {
+            switch (j) {
+                case 'U':
+                    y--;
+                    break;
+                case 'D':
+                    y++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+
+            ymax = std::max(y, ymax);
+            ymin = std::min(x, xmin);
+            xmax = std::max(x, xmax);
+            xmin = std::min(x, xmin);
+
+            for (const auto& [py, px] : obstacle) {
+                int qy = py - y, qx = px - x;
+                if (0 <= qy && qy < n && 0 <= qx && qx < n) {
+                    edge_map[qy][qx][i] = 0;
+                }
+            }
+        }
+
+        for (int py = 0; py < n; py++) {
+            for (int px = 0; px < n; px++) {
+                if (py + ymin < 0 || py + ymax >= n || px + xmin < 0 ||
+                    px + xmax >= n) {
+                    edge_map[py][px][i] = 0;
+                }
+            }
+        }
+    }
+}
