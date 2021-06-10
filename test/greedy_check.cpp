df4bf003925bb9a50b67bc46d6b70a03aef67c69
Mitarushi <s153@D13.nada.local>
2021-06-10 16:28:35 +0900

    fix 1-indexed

--- test/greedy_check.cpp
+++ test/greedy_check.cpp
@@ -34,7 +34,7 @@ int main() {
                 max = result;
             }
         }
-        use.push_back(max_i);
+        use.push_back(max_i + 1);
         std::tie(max, visited) = std::move(reach_count(visited, use, max_i));
         cnt++;
         // printf("cnt: %d, idx: %d,  count: %d, need: %d\n", cnt, max_i, max, need_count);
