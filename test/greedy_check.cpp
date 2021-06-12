6cc52229d0d41530701ce57d50367ad3009e1bc8
Mitarushi <60428311+Mitarushi@users.noreply.github.com>
2021-06-13 01:13:59 +0900

    mini beam search

--- test/greedy_check.cpp
+++ test/greedy_check.cpp
@@ -19,33 +19,62 @@ int main() {
     }
     printf("YES\n");
 
-    std::vector<int> use;
-    std::vector<std::vector<char>> visited(n, std::vector(n, (char) 0));
-    visited[0][0] = 1;
+    const int beam_width = 30;
+
+    std::vector<std::vector<int>> beam_use = {{}}, next_beam_use;
+    std::vector<std::vector<std::vector<char>>> beam_visited = {std::vector(n, std::vector(n, (char) 0))},
+            next_beam_visited;
+    std::vector<int> beam_count = {1}, next_beam_count;
+    beam_visited[0][0][0] = 1;
 
     int cnt = 0;
     while (true) {
-        int max_i, max = -1;
-        for (int i = 0; i < m; i++) {
-            int result = predict(i, visited);
+        std::vector<std::tuple<int, int, int>> next;
+        next_beam_use.clear();
+        next_beam_visited.clear();
+        next_beam_count.clear();
 
-            if (result > max) {
-                max_i = i;
-                max = result;
+        for (int b = 0; b < beam_use.size(); b++) {
+            for (int i = 0; i < m; i++) {
+                int result = predict(i, beam_visited[b]) + beam_count[b];
+
+                next.emplace_back(result, b, i);
             }
         }
-        
-        std::tie(max, visited) = std::move(reach_count(visited, use, max_i));
-        cnt++;
-        use.push_back(max_i);
-        // printf("cnt: %d, idx: %d,  count: %d, need: %d\n", cnt, max_i, max, need_count);
-        // fflush(stdout);
 
-        if (max == need_count) {
+        std::sort(next.begin(), next.end(), std::greater<>());
+        cnt++;
+
+        int next_beam_size = std::min(beam_width, (int) next.size());
+        next_beam_use.resize(next_beam_size);
+        next_beam_visited.resize(next_beam_size);
+        next_beam_count.resize(next_beam_size);
+
+        for (int b = 0; b < next_beam_size; b++) {
+            auto[_, i, j] = next[b];
+
+            std::tie(next_beam_count[b], next_beam_visited[b]) =
+                    std::move(reach_count(beam_visited[i], beam_use[i], j));
+
+            next_beam_use[b] = beam_use[i];
+            next_beam_use[b].push_back(j);
+        }
+
+        auto max_element = std::max_element(next_beam_count.begin(), next_beam_count.end());
+
+//        printf("cnt: %d, count: %d, need: %d\n", cnt, *max_element, need_count);
+//        fflush(stdout);
+
+        if (*max_element == need_count) {
+            printf("%d\n", cnt);
+            line_print(next_beam_use[max_element - next_beam_count.begin()], 1);
             break;
         }
+
+        std::swap(beam_use, next_beam_use);
+        std::swap(beam_visited, next_beam_visited);
+        std::swap(beam_count, next_beam_count);
     }
 
-    printf("%d\n", cnt);
-    line_print(use, 1);
+
 }
